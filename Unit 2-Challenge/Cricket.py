09fb18ac892ed0ed248fa362dab92af499da4f15
Sivaaram <126768351+Sivaaram@users.noreply.github.com>
2023-09-19 18:59:46 +0530

    Cricket.py

--- Unit 2-Challenge/Cricket.py
+++ Unit 2-Challenge/Cricket.py
@@ -1,18 +1,13 @@
-#Base Class
 class Player:
   def play(self):
     print("The Player is playing Cricket")
-#Derived Class
 class Batsman(Player):
   def play(self):
    print("The Batsman is Batting")
-#Derived Class
 class Bowler(Player):
   def play(self):
    print("The Bowler is Bowling")
-#Object Creation
 batsman=Batsman()
 bowler=Bowler()
-#Method Calling
 batsman.play()
 bowler.play()
